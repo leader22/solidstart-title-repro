edb917e9b34e6fed4297d69bbfca5dd503695b0f
Yuji Sugiura <y.sugiura.0316@gmail.com>
2023-03-17 09:11:29 +0900

    Init repro

---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import { Title } from "solid-start";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>{"🐢"} | XXX</Title>
+      <h1>Hello world!</h1>
+    </main>
+  );
+}
